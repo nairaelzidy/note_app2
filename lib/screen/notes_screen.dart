ea2aa0c158bc4024361401cc3792d539c790ee46
Your Name <you@example.com>
2024-02-19 20:17:33 +0200

    create new folder

---
+++ lib/screen/notes_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class NotesScreen extends StatelessWidget {
+  const NotesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
