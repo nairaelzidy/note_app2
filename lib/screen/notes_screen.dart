5ee3e9a8f45b66f083b592af8e71442f707bde48
Your Name <you@example.com>
2024-02-19 20:33:46 +0200

    change theme and creat notesscreen

---
+++ lib/screen/notes_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NotesScreen extends StatelessWidget {
+  const NotesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
